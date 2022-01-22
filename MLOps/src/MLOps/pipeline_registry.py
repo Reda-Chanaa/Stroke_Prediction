3dc12eace933ab56a85f071d8dddf1106f8fb194
Reda Chanaa <chanaa-reda@hotmail.fr>
2022-01-22 17:17:34 +0100

    Edit pipeline registry

--- MLOps/src/MLOps/pipeline_registry.py
+++ MLOps/src/MLOps/pipeline_registry.py
@@ -2,12 +2,21 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
-
+from MLOps.pipelines import data_science as ds
+from MLOps.pipelines import data_engineering as de
 
 def register_pipelines() -> Dict[str, Pipeline]:
-    """Register the project's pipelines.
+    """Register the project's pipeline.
 
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
+
     """
-    return {"__default__": Pipeline([])}
+    data_processing_pipeline = de.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+
+    return {
+        "__default__": data_processing_pipeline + data_science_pipeline,
+        "dp": data_processing_pipeline,
+        "ds": data_science_pipeline,
+    }
