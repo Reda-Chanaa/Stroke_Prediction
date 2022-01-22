98ee8adedf712b6777b0887511f87532b76b5a9e
Reda Chanaa <chanaa-reda@hotmail.fr>
2022-01-22 17:18:58 +0100

    Add pipelines for data engineering nodes

--- MLOps/src/MLOps/pipelines/data_engineering/pipeline.py
+++ MLOps/src/MLOps/pipelines/data_engineering/pipeline.py
@@ -1,7 +1,6 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import limit_data_size
-from .nodes import rename_column
+from .nodes import limit_data_size, missing_values,rename_columns, summarize
 
 def create_pipeline(**kwargs):
     # Create the pipeline that will transfer the appropriate
@@ -15,10 +14,22 @@ def create_pipeline(**kwargs):
                 name="limit_data_size"
                 ),
             node(
-                func=rename_column,
+                func=rename_columns,
                 inputs="strokeData",
                 outputs="strokeData_renamed",
-                name="rename_column"
+                name="rename_columns"
+                ),
+            node(
+                func=missing_values,
+                inputs="strokeData_renamed",
+                outputs="strokeData_missing",
+                name="missing_values"
+                ),
+            node(
+                func=summarize,
+                inputs="strokeData_renamed",
+                outputs="strokeData_summarized",
+                name="summarize"
                 ),
         ]
         )
