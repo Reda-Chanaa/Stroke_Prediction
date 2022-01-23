6a0f17778a15d6a58db65ab398ecd0a3a5f9a750
Reda Chanaa <chanaa-reda@hotmail.fr>
2022-01-23 03:17:11 +0100

    Update Data science Pipline

--- MLOps/src/MLOps/pipelines/data_science/pipeline.py
+++ MLOps/src/MLOps/pipelines/data_science/pipeline.py
@@ -1,8 +1,10 @@
 from kedro.pipeline import Pipeline, node
 
 from .nodes.split_data import split_data
-from .nodes.LinearRegression import Linear_Regression
 from .nodes.LogisticRegression import Logistic_Regression
+from .nodes.pycaret import pycaret_classification
+from .nodes.AdaBoost import AB_Classifier
+from .nodes.SVC import SVC_Classifier
 
 def create_pipeline(**kwargs):
     return Pipeline(
@@ -14,16 +16,31 @@ def create_pipeline(**kwargs):
                 name="model_input",
             ),
             node(
-                func=Linear_Regression,
-                inputs=["X_train", "X_test", "y_train", "y_test"],
-                outputs="LinearRegressor",
-                name="LinearRegressor",
-            ),
-            node(
                 func=Logistic_Regression,
                 inputs=["X_train", "X_test", "y_train", "y_test"],
                 outputs="LogisticRegressor",
                 name="LogisticRegressor",
             ),
+            node(
+                func=AB_Classifier,
+                inputs=["X_train", "X_test", "y_train", "y_test"],
+                outputs="AdaClassifier",
+                name="AdaClassifier",
+            ),
+            
         ]
     )
+
+"""
+            node(
+                func=pycaret_classification,
+                inputs=["strokeData_encoded"],
+                outputs="pycaret_classification",
+                name="pycaret_classification",
+            ),
+            node(
+                func=SVC_Classifier,
+                inputs=["X_train", "X_test", "y_train", "y_test"],
+                outputs="SVC_CLaassifier",
+                name="SVC_CLaassifier",
+            ),"""
